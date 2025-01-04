de4445ccfa2f2008564717671382a54f9658e5db
geun02k <geun02k@gmail.com>
2025-01-04 17:40:19 +0900

    feat : [003-2-SIGN-IN-ENCODING-UTIL] JWT 토큰 생성 시 사용할 sercret key를 위한 AES256 암,복호화 util 생성
    해당 브랜치는 추후 JWT 토큰 secret key를 외부에서 생성해 등록하지 않고 프로그램 내부에서 암호화하기 위해 개발중으로 미완성.

---
+++ src/main/java/com/shop/reservation/util/Aes256Util.java
@@ -0,0 +1,49 @@
+package com.shop.reservation.util;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+public class Aes256Util { // 암호화방식지정
+
+    // 자바에서 지원하는 암호화 알고리즘 중 하나.
+    public static String alg = "AES/CBC/PKCS5Padding";
+    private static final String KEY = "RESERVATIONPRJSECRETKEY1";
+    private static final String IV = KEY.substring(0,16);
+
+    // 암호화
+    public static String encrypt(String plainText) {
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8));
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
+
+            byte[] encrypted = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+            return Base64.encodeBase64String(encrypted);
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    // 복호화
+    public static String decrypt(String cipherText) {
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8));
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
+
+            byte[] decodeBytes = Base64.decodeBase64(cipherText);
+            byte[] decrypted = cipher.doFinal(decodeBytes);
+            return new String(decrypted, StandardCharsets.UTF_8);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
