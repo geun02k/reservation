7e95ccf853365f13dbc14f4c6f5ca5fefd1dca7b
geun02k <geun02k@gmail.com>
2025-01-06 10:52:03 +0900

    feat : [006-SHOP-REMOVE] 매장삭제 api 생성 (매장관리자 권한)

--- src/main/java/com/shop/reservation/exception/type/ShopErrorCode.java
+++ src/main/java/com/shop/reservation/exception/type/ShopErrorCode.java
@@ -14,6 +14,8 @@ public enum ShopErrorCode implements CommonErrorCode {
     ALREADY_REGISTERED_SHOP(HttpStatus.BAD_REQUEST.value(), "이미 등록된 매장입니다."),
     NOT_REGISTERED_SHOP(HttpStatus.BAD_REQUEST.value(), "등록되지 않은 매장입니다."),
     NOT_SHOP_IN_CHARGE(HttpStatus.BAD_REQUEST.value(), "담당 매장이 아닙니다."),
+    REMOVED_SHOP(HttpStatus.BAD_REQUEST.value(), "삭제된 매장입니다."),
+    FAIL_REMOVE_SHOP(HttpStatus.INTERNAL_SERVER_ERROR.value(), "매장이 삭제되지 않았습니다."),
 
     INVALID_TEL_NUMBER(HttpStatus.BAD_REQUEST.value(), "유효하지 않은 전화번호입니다."),
     LIMIT_NAME_CHARACTERS_FROM_1_TO_100(HttpStatus.BAD_REQUEST.value(), "매장명 길이는 최소 1자, 최대 100자로 제한됩니다."),
