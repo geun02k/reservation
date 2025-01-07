5d7c045196a33569f5ba246d650523324574cc03
geun02k <geun02k@gmail.com>
2025-01-07 22:20:48 +0900

    refactor : [008-REFACTORING] SignUpException.java getter() -> @Getter 어노테이션으로 대체 및 statusCode 필드추가

--- src/main/java/com/shop/reservation/exception/SignUpException.java
+++ src/main/java/com/shop/reservation/exception/SignUpException.java
@@ -1,30 +1,19 @@
 package com.shop.reservation.exception;
 
 import com.shop.reservation.exception.type.SignUpErrorCode;
+import lombok.Getter;
 
+@Getter
 public class SignUpException extends BaseAbstractException {
 
     private final SignUpErrorCode errorCode;
     private final String errorMessage;
+    private final int statusCode;
 
     public SignUpException(SignUpErrorCode errorCode) {
         this.errorCode = errorCode;
         this.errorMessage = errorCode.getErrorMessage();
-    }
-
-    @Override
-    public SignUpErrorCode getErrorCode() {
-        return this.errorCode;
-    }
-
-    @Override
-    public String getErrorMessage() {
-        return this.errorMessage;
-    }
-
-    @Override
-    public int getStatusCode() {
-        return errorCode.getStatusCode();
+        this.statusCode = errorCode.getStatusCode();
     }
 
 }
