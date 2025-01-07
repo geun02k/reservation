ea8dd0a837c604c811247468f765dc4b12b1b048
geun02k <geun02k@gmail.com>
2025-01-07 21:33:02 +0900

    bug : [006-SHOP-LIST-SEARCH] Page 인터페이스의 직렬화 문제 해결을 위해 @EnableSpringDataWebSupport 어노테이션 추가

--- src/main/java/com/shop/reservation/config/AppConfig.java
+++ src/main/java/com/shop/reservation/config/AppConfig.java
@@ -2,10 +2,14 @@ package com.shop.reservation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO) // 데이터 직렬화 문제로 인해 추가
 public class AppConfig {
 
     // 비밀번호 암호화 구현체 정의 및 빈 등록.
